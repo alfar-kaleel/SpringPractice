a8e3eca2057016072afa29e06d95e6eafefe04c1
unknown <alfarkaleel@gmail.com>
2020-02-04 11:44:40 +0530

    second commit

---
+++ AnnotationBasedMemberVariableInjection/src/com/alfar/training/salesmanager/service/EmployeeServiceImp.java
@@ -0,0 +1,62 @@
+package com.alfar.training.salesmanager.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alfar.training.salesmanager.model.Employee;
+import com.alfar.training.salesmanager.repository.EmployeeRepository;
+import com.alfar.training.salesmanager.repository.HibernateEmployeeRepositoryImp;
+
+
+@Service("employeeService")
+ public class EmployeeServiceImp implements EmployeeService {
+	
+	
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	
+	public EmployeeServiceImp() {
+		
+		System.out.println("Default constructor executed");
+	}
+	
+	
+	
+	public EmployeeServiceImp(EmployeeRepository employeeRepository) {
+		
+		
+		this.employeeRepository = employeeRepository;
+		System.out.println("Overload constructor executed");
+	}
+	
+	
+	
+	public EmployeeRepository getEmployeeRepository() {
+		
+		
+		return employeeRepository;
+	}
+	
+
+	public void setEmployeeRepository(EmployeeRepository emprep) {
+		
+		
+		this.employeeRepository = emprep;
+		System.out.println("Setter Exceuted");
+		
+	}
+	
+	
+	
+	public List<Employee> getAllEmployees(){
+		
+		
+		
+		return employeeRepository.getAllEmployees();
+	}
+
+}
---
+++ AnnotationBasedSetterInjection/src/com/alfar/training/salesmanager/model/Employee.java
@@ -0,0 +1,30 @@
+package com.alfar.training.salesmanager.model;
+
+public class Employee {
+	
+	private String employeeName;
+	private String employeeLocation;
+	
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	
+	
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+
+	public String getEmployeeLocation() {
+		return employeeLocation;
+	}
+
+
+	public void setEmployeeLocation(String employeeLocation) {
+		this.employeeLocation = employeeLocation;
+	}
+	
+	
+
+}
